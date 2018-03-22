300e97ca2be53d09c460691ed739fcda47a4e3ae
dawiddc <dawid.ciok@gmail.com>
2018-03-22 19:08:27 +0100

    Add Proxy Connection Setup

--- ProxyServerTPSI/src/com/dawiddc/main/ProxyServer.java
+++ ProxyServerTPSI/src/com/dawiddc/main/ProxyServer.java
@@ -1,49 +1,67 @@
-package com.dciok.main;
+package com.dawiddc.main;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.URL;
 
 public class ProxyServer {
     private static final int port = 8000;
-    private final HttpServer server;
-    private ProxyServer() throws IOException {
-        server = HttpServer.create(new InetSocketAddress(port), 0);
-    }
-
-    private HttpServer getProxyServer(){ return this.server; }
-
+    private static HttpServer server;
 
     public static void main(String[] args) throws Exception {
-        ProxyServer proxyServer = new ProxyServer();
-        proxyServer.getProxyServer().createContext("/", new RootHandler());
+        server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new RootHandler());
     }
 
-    private static class RootHandler implements HttpHandler {
 
+    private static class RootHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange httpExchange) {
             HttpURLConnection connection = null;
             try {
-//            httpExchange.getRequestBody();
-                Headers requestHeaders = httpExchange.getRequestHeaders();
-                URL url = httpExchange.getRequestURI().toURL();
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setInstanceFollowRedirects(false);
-                connection.setRequestMethod(httpExchange.getRequestMethod());
-                connection.setRequestProperty("Via", server.getAddress());
+                connection = setupConnection(httpExchange);
+                byte[] requestBytes = readRequestBodyToByteArray(httpExchange.getRequestBody());
+                OutputStream os = connection.getOutputStream();
+                os.write(requestBytes);
+                os.close();
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 connection.disconnect();
             }
+        }
 
+        private byte[] readRequestBodyToByteArray(InputStream requestBody) throws Exception {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            int nRead;
+            byte[] data = new byte[16384];
+            while ((nRead = requestBody.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+            return buffer.toByteArray();
+        }
+
+        private HttpURLConnection setupConnection(HttpExchange httpExchange) throws Exception {
+            HttpURLConnection connection = null;
+            URL url = httpExchange.getRequestURI().toURL();
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setInstanceFollowRedirects(false);
+            connection.setRequestMethod(httpExchange.getRequestMethod());
+            connection.setRequestProperty("Via", server.getAddress().getHostString());
+            Headers requestHeaders = httpExchange.getRequestHeaders();
+            for (String key : requestHeaders.keySet()) {
+                connection.setRequestProperty(key, requestHeaders.get(key).get(0));
+            }
+            return connection;
         }
     }
 }
